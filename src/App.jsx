74c0c2068734f5513451317b1c668bbe70d37a7b
Andrii D. <and05gt@gmail.com>
2025-03-05 19:58:36 +0100

    Import fonts and added routing to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import "./App.css";
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage.jsx';
+import Layout from './components/Layout.jsx';
 
-function App() {
-  return <></>;
-}
+const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+const PsychologistsPage = lazy(() =>
+  import('./pages/PsychologistsPage/PsychologistsPage.jsx'),
+);
+const FavoritesPage = lazy(() =>
+  import('./pages/FavoritesPage/FavoritesPage.jsx'),
+);
+
+const App = () => {
+  return (
+    <Suspense fallback={<h3>Loading...</h3>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/psychologists" element={<PsychologistsPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
+  );
+};
 
 export default App;
